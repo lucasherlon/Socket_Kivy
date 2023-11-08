69e5212c0a50b27adff48d75e1b6d12ada43cf1d
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2023-11-08 11:56:42 -0300

    Comentando o codigo

--- main.py
+++ main.py
@@ -5,40 +5,40 @@ import socket
 
 class Caixa(BoxLayout):
     def enviar(self):
-        # Create a socket object
+        # Cria socket
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Connect to the server
+        #Conecta com lado servidor
         host = '127.0.0.1'  # The server's IP address
         port = 12345  # The server's port
         client_socket.connect((host, port))
 
-        # Send data to the server
+        # Envio dos dados
         message = self.ids.pr1.text
         client_socket.send(message.encode('utf-8'))
 
-        # Close the socket
+        #Fechamento do socket
         client_socket.close()
 
     def receber(self):
-        # Create a socket object
+        # Cria socket
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        # Bind the socket to a specific address and port
-        host = '127.0.0.1'  # Your server's IP address
-        port = 12345  # Choose a free port
+        #Seta a porta de escuta e o host
+        host = '127.0.0.1'  
+        port = 12345  
         server_socket.bind((host, port))
 
-        # Listen for incoming connections
+        # Escuta potenciais conexoes
         server_socket.listen(5)
 
-        # Accept a connection and establish a connection object
+        # Aceite da conexao e recebimento dos dados
         client_socket, addr = server_socket.accept()
         data = client_socket.recv(1024)
         print(f"Connection from {addr} Mensagem: {data.decode('utf-8')}")
         self.ids.p1.text = f"Connection from {addr} Mensagem: {data.decode('utf-8')}"
 
-        # Close the sockets
+        # Fechamento dos sockets
         client_socket.close()
         server_socket.close()
 
